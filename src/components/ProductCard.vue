22d7e6ada55a79607e5ccd227f658e73af2ccb19
SimoneCerri <simone.cerri1994@gmail.com>
2024-03-22 12:08:35 +0100

    add data.js file and refactoring ProductCard with a v-for cycle

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default
+    {
+        name: "Card",
+        props:
+        {
+            product: Object
+        }
+    }
+</script>
+
+<template>
+    <div class="card">
+        <img :src="product.image" alt="">
+        <span v-if="product.discount" class="discount">-50%</span>
+        <span v-if="product.sustenability" class="sustain">Sostenibilità</span>
+        <span class="hearts">&hearts;</span> <br>
+        <span class="brand">{{product.brand}}</span> <br>
+        <span>
+            <strong>
+                {{product.name}}
+            </strong>
+        </span> <br>
+        <span class="new_price">{{product.price}}&euro;</span>
+        <span v-if="product.discount" class="old_price">{{ product.priceDiscounted }}&euro;</span>
+    </div>
+</template>
+
+<style lang="scss"></style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,34 +1,26 @@
 <script>
-import CardMain_1 from "./CardMain_1.vue";
-import CardMain_2 from "./CardMain_2.vue";
-import CardMain_3 from "./CardMain_3.vue";
-import CardMain_4 from "./CardMain_4.vue";
-import CardMain_5 from "./CardMain_5.vue";
-import CardMain_6 from "./CardMain_6.vue";
+import {products} from "../data.js"
+import ProductCard from "./ProductCard.vue"
 
 export default
 {
     name: "SiteMain",
     components:
     {
-        CardMain_1,
-        CardMain_2,
-        CardMain_3,
-        CardMain_4,
-        CardMain_5,
-        CardMain_6,
+        ProductCard
+    },
+    data()
+    {
+        return{
+            products
+        }
     }
 }
 </script>
 
 <template>
     <main id="site_main">
-        <CardMain_1 />
-        <CardMain_2 />
-        <CardMain_3 />
-        <CardMain_4 />
-        <CardMain_5 />
-        <CardMain_6 />
+        <ProductCard v-for="product in products" :product="product" :key="product.id" />
     </main>
     <!-- /#site_main -->
 </template>
