e015ed8520eab02259db4060739b7b57a1107233
SimoneCerri <simone.cerri1994@gmail.com>
2024-03-29 11:06:37 +0100

    move the click-emit on card's info instead of only name

--- src/components/main/ProductCard.vue
+++ src/components/main/ProductCard.vue
@@ -9,7 +9,7 @@ export default
         },
         methods:
         {
-            
+
         }
     }
 </script>
@@ -20,19 +20,19 @@ export default
         <span v-if="product.discount" class="discount">-50%</span>
         <span v-if="product.sustenability" class="sustain">Sostenibilità</span>
         <span class="hearts">&hearts;</span> <br>
-        <div class="info">
-           <span class="brand">{{product.brand}}</span> <br>
-        <span>
-            <strong @click="$emit('show-modal', product)">
-                {{product.name}}
-            </strong>
-        </span> <br>
-        <span class="new_price">{{product.price}}&euro;</span>
-        <span v-if="product.discount" class="old_price">{{ product.priceDiscounted }}&euro;</span> 
+        <div class="info" @click="$emit('show-modal', product)">
+            <span class="brand">{{ product.brand }}</span>
+            <br>
+            <span>
+                <strong>
+                    {{ product.name }}
+                </strong>
+            </span>
+            <br>
+            <span class="new_price">{{ product.price }}&euro;</span>
+            <span v-if="product.discount" class="old_price">{{ product.priceDiscounted }}&euro;</span>
         </div>
     </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
